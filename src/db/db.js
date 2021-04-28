507533c1eb744ddc42261c419152fa2e0e7e3874
eleanorjw <74851375+eleanorjw@users.noreply.github.com>
2021-04-28 08:14:57 +0800

    Update db.js

--- src/db/db.js
+++ src/db/db.js
@@ -16,11 +16,13 @@ export class db extends Dexie {
     this.cards = this.table("cards");
   }
 
+  // get all project name
   async getProjects() {
     let names = await this.projects.orderBy("id").toArray();
     return names;
   }
-
+  
+  // get all cards of selected project
   async getCards(currentProject) {
     let status0 = this.cards.where({ project: currentProject, status: 0 });
     status0 = await status0.toArray();
@@ -32,12 +34,14 @@ export class db extends Dexie {
     status3 = await status3.toArray();
     return [{ status0 }, { status1 }, { status2 }, { status3 }];
   }
-
+  
+  // check if project name exited
   async checkProjectName(projectname) {
     let num = this.projects.where({ name: projectname });
     return await num.count();
   }
-
+  
+  // add new project to table
   async addProject(projectname) {
     let x = await this.checkProjectName(projectname);
     if (x != 0) {
@@ -47,6 +51,7 @@ export class db extends Dexie {
     }
   }
 
+  // add new card to selected project
   addCard(e, x) {
     if (x == null) {
       alert("Please select project");
@@ -62,6 +67,8 @@ export class db extends Dexie {
       });
     }
   }
+  
+  // update card with important key
   async important(e) {
     let i = await this.cards.get(e);
     if (i.important == 1) {
@@ -71,20 +78,24 @@ export class db extends Dexie {
     }
   }
 
+  // increment status
   async next(e) {
     let s = await this.cards.get(e);
     return this.cards.update(e, { status: s.status + 1 });
   }
 
+  // delete card by id
   deleteCard(cardId) {
     return this.cards.delete(cardId);
   }
 
+  // delete all cards when project is deleted
   async deleteAllCards(name) {
     let cards = this.cards.where({ project: name });
     return await cards.delete();
   }
 
+  // delete project
   async deleteProject(name) {
     this.deleteAllCards(name);
     let project = this.projects.where({ name: name });
